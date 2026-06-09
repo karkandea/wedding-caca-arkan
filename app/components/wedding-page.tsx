8e3fe3a258d3d06d75ddbee6a56a999622ac5e3f
karkandea <karkandea@gmail.com>
2026-06-09 12:10:46 +0700

    Remove back button

--- app/components/wedding-page.tsx
+++ app/components/wedding-page.tsx
@@ -1,4 +1,3 @@
-import BackButton from "./back-button";
 import DeferredWeddingSections from "./deferred-wedding-sections";
 import FloatingSectionNavLoader from "./floating-section-nav-loader";
 import NewHeroSection from "./new-hero-section";
@@ -12,7 +11,6 @@ export default function WeddingPage({ guestName }: WeddingPageProps) {
   return (
     <>
       <ScrollToHeroOnLoad />
-      <BackButton />
       <FloatingSectionNavLoader />
       <main className="flex flex-1 flex-col">
         <NewHeroSection />
